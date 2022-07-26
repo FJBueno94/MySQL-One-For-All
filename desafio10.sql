c6242e8ee93aff513df87fe16883b7c51014ab97
Fernando Bueno <fernando_jordi@hotmail.com>
2022-07-25 21:40:40 -0300

    corrige para uppercase

--- desafio10.sql
+++ desafio10.sql
@@ -9,5 +9,5 @@ ON h.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.planos AS p
 ON u.plano_id = p.plano_id
 WHERE p.plano = "gratuito" OR p.plano = "pessoal"
-GROUP BY nome
+GROUP BY NOME
 ORDER BY NOME;
